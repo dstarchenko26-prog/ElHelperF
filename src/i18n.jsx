ded8fef54a42afcac0ed72a67f62d3763b19d68f
dstarchenko26-prog <denys.starchenko.oi.2023@lpnu.ua>
2026-01-05 03:38:20 +0200

    Update i18n.jsx

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -3,19 +3,48 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
+import ukTranslation from './locales/uk/uk.json';
+import enTranslation from './locales/en/en.json';
+
+const resources = {
+  en: {
+    translation: enTranslation
+  },
+  uk: {
+    translation: ukTranslation
+  }
+};
+
 i18n
-  .use(Backend)
+  // .use(Backend) <-- 2. Видаляємо це (Backend більше не треба)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources,
     fallbackLng: 'uk',
-    debug: false,
-    backend: {
-      loadPath: `${import.meta.env.BASE_URL}locales/{{lng}}/translation.json`,
+    
+    react: {
+        useSuspense: false 
     },
+    
     interpolation: {
-      escapeValue: false,
-    },
+      escapeValue: false
+    }
   });
+
+// i18n
+//   .use(Backend)
+//   .use(LanguageDetector)
+//   .use(initReactI18next)
+//   .init({
+//     fallbackLng: 'uk',
+//     debug: false,
+//     backend: {
+//       loadPath: `${import.meta.env.BASE_URL}locales/{{lng}}/translation.json`,
+//     },
+//     interpolation: {
+//       escapeValue: false,
+//     },
+//   });
 
 export default i18n;
